6578f8193e6ee03a6803297755f000da56cca310
Paul Grevink <paul.grevink@gmail.com>
2018-12-24 20:19:41 +0100

    24-12-2018 portgroups

---
+++ Network/VDS-Version.Vester.ps1
@@ -0,0 +1,26 @@
+﻿# Test file for the Vester module - https://github.com/WahlNetwork/Vester
+# Called via Invoke-Pester VesterTemplate.Tests.ps1
+
+# Test title, e.g. 'DNS Servers'
+$Title = 'VDS Version'
+
+# Test description: How New-VesterConfig explains this value to the user
+$Description = 'Get the version of the VDS'
+
+# The config entry stating the desired values
+$Desired = $cfg.vds.version
+
+# The test value's data type, to help with conversion: bool/string/int
+$Type = 'string'
+
+# The command(s) to pull the actual value for comparison
+# $Object will scope to the folder this test is in (Cluster, Host, etc.)
+[ScriptBlock]$Actual = {
+    $Object.Version
+}
+
+# The command(s) to match the environment to the config
+# Use $Object to help filter, and $Desired to set the correct value
+[ScriptBlock]$Fix = {
+    Write-Host "No automated Remediation for VDS version"
+}
