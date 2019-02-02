af70aa4b6a7600dcfdfa5cfc8fc2c9ede0d958dd
Paul Grevink <paul.grevink@gmail.com>
2019-02-02 14:39:29 +0100

    201902021439

--- gitpush.ps1
+++
@@ -1,5 +0,0 @@
-﻿cd 'C:\Program Files\WindowsPowerShell\Modules\Vester\1.2.0\Tests'
-git add * 
-git commit -m "my first commit"
-git push --repo https://github.com/PaulGrevink/tests.git origin master
-git log
